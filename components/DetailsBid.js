a8d2233fc4e67e9c56a80dd67fc0289bd9d393e2
Abbas Anandwala <abbasa5251@hotmail.com>
2022-09-12 16:17:47 +0530

    Init commit

---
+++ components/DetailsBid.js
@@ -0,0 +1,49 @@
+import { Text, View, Image } from "react-native";
+import React from "react";
+
+import { ETHPrice } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
+
+const DetailsBid = ({ bid }) => {
+	return (
+		<View
+			style={{
+				width: "100%",
+				flexDirection: "row",
+				justifyContent: "space-between",
+				alignItems: "center",
+				marginVertical: SIZES.base,
+				paddingHorizontal: SIZES.base * 2,
+			}}
+		>
+			<Image
+				source={bid.image}
+				resizeMode="contain"
+				style={{ width: 48, height: 48 }}
+			/>
+			<View>
+				<Text
+					style={{
+						fontSize: SIZES.small,
+						fontFamily: FONTS.semiBold,
+						color: COLORS.primary,
+					}}
+				>
+					Bid palced by {bid.name}
+				</Text>
+				<Text
+					style={{
+						fontSize: SIZES.small - 2,
+						fontFamily: FONTS.regular,
+						color: COLORS.secondary,
+					}}
+				>
+					{bid.date}
+				</Text>
+			</View>
+			<ETHPrice price={bid.price} />
+		</View>
+	);
+};
+
+export default DetailsBid;
